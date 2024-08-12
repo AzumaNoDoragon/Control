a43e0fc2bdecdc18ced06c4aba8e14cff318a9f2
Gabriel Afini <gabrielsads@gmail.com>
2024-08-12 13:54:54 -0300

    Update commit

--- Control.py
+++ Control.py
@@ -2,9 +2,13 @@ import subprocess
 import tkinter as tk
 from tkinter import messagebox
 
+def root(text_area, insertText):
+    text_area.delete("1.0", tk.END) 
+    text_area.insert(tk.END, insertText)
+
 def desligar():
     try:
-        tempo = int(visor.get("1.0", tk.END).strip())
+        tempo = int(textArea.get("1.0", tk.END).strip())
         minutos = tempo // 60
         resposta = messagebox.askyesno("Confirmação", f"Você deseja desligar seu computador em {minutos} minutos?")
         if resposta:
@@ -15,7 +19,7 @@ def desligar():
 
 def reiniciar():
     try:
-        tempo = int(visor.get("1.0", tk.END).strip())
+        tempo = int(textArea.get("1.0", tk.END).strip())
         minutos = tempo // 60
         resposta = messagebox.askyesno("Confirmação", f"Você deseja reiniciar seu computador em {minutos} minutos?")
         if resposta:
@@ -30,24 +34,35 @@ def cancelar():
     if resultado.returncode != 0:
         messagebox.showerror("Erro", "Não foi possível anular o desligamento do sistema porque o sistema não estava sendo desligado.")
 
+B_width0 = 15
+
 B_width = 23
 B_height = 2
 
 ct = tk.Tk()
 ct.title("Control")
-ct.geometry("400x150+200+250")
+ct.geometry("400x190+200+250")
 ct.resizable(False, False)
 
 container = tk.Frame(ct)
 container.pack(padx=0, pady=5)
-visor = tk.Text(container, height=1, width=20, font=("Arial", 23), borderwidth=5)
-visor.grid(row=0, column=0, padx=0, pady=0)
+textArea = tk.Text(container, height=1, width=20, font=("Arial", 23), borderwidth=5)
+textArea.grid(row=0, column=0, padx=0, pady=0)
 
-containerButton = tk.Frame(ct)
-containerButton.pack(padx=0, pady=0)
-buttonDesliga = tk.Button(containerButton, text="Desligar", width=B_width, height=B_height, command=desligar)
+containerButton0 = tk.Frame(ct)
+containerButton0.pack(padx=0, pady=0)
+button10 = tk.Button(containerButton0, text="10 minutos!", width=B_width0, height=B_height, command=lambda: root(textArea, "600"))
+button10.grid(row=0, column=0, padx=0, pady=0)
+button30 = tk.Button(containerButton0, text="30 minutos!", width=B_width0, height=B_height, command=lambda: root(textArea, "1800"))
+button30.grid(row=0, column=1, padx=0, pady=0)
+button60 = tk.Button(containerButton0, text="60 minutos!", width=B_width0, height=B_height, command=lambda: root(textArea, "3600"))
+button60.grid(row=0, column=2, padx=0, pady=0)
+
+containerButton1 = tk.Frame(ct)
+containerButton1.pack(padx=0, pady=0)
+buttonDesliga = tk.Button(containerButton1, text="Desligar", width=B_width, height=B_height, command=desligar)
 buttonDesliga.grid(row=0, column=0, padx=0, pady=0)
-buttonReiniciar = tk.Button(containerButton, text="Reiniciar", width=B_width, height=B_height, command=reiniciar)
+buttonReiniciar = tk.Button(containerButton1, text="Reiniciar", width=B_width, height=B_height, command=reiniciar)
 buttonReiniciar.grid(row=0, column=1, padx=0, pady=0)
 
 containerButtonCancel = tk.Frame(ct)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Control
-Aplicativo GUI em Python para agendar desligamento ou reinício do computador. Com uma interface simples em Tkinter, insira o tempo em segundos, confirme a ação e a aplicação toma conta do restante!
+Aplicativo GUI em Python para agendar desligamento ou reinício do computador. Com uma interface simples em Tkinter, insira o tempo em segundos ou selecione alguma das opções, confirme a ação e a aplicação toma conta do restante, caso tenha inserido o valor incorreto ou não deseja mais desligar/reiniciar o computador, clique em cancelar, esse botão serve apenas para cancelar uma ação!
 
 Tenha python instalado, rode o "install_libraries.cmd" para instalar as bibliotecas o mesmo irá perguntar se deseja iniciar a aplicação, posteriomente pode ser iniciao pelo run_script.vbs
